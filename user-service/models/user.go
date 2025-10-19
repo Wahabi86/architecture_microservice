e8001bb3c8acb7445654e8281604ae62fe40ad7e
Christian Putri <christianptrii@gmail.com>
2025-10-19 14:41:58 +0800

    add user models with necessary fields

---
+++ user-service/models/user.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type User struct {
+	ID                    uint       `gorm:"primaryKey" json:"id"`
+	Name                  string     `gorm:"type:varchar(100)" json:"name"`
+	Email                 string     `gorm:"uniqueIndex;type:varchar(100)" json:"email"`
+	PasswordHash          string     `gorm:"type:text" json:"-"`
+	SubscriptionType      string     `gorm:"type:varchar(50);default:'none'" json:"subscription_type"`
+	SubscriptionExpiredAt *time.Time `json:"subscription_expired_at"`
+	Watchlist             []Movie    `gorm:"many2many:watchlists;" json:"-"`
+}
+
+type Movie struct {
+	ID uint `gorm:"primaryKey"`
+}
